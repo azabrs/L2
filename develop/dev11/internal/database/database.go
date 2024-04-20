94cbbc98b4c5b9c17a3d7135021958e7e4fd8cb9
azabrs <Apukha@mail.ru>
2024-04-20 15:55:03 +0300

    dev11: add config

---
+++ develop/dev11/internal/database/database.go
@@ -0,0 +1,12 @@
+package database
+
+type Database struct{
+	port string
+	DbName string
+	user string
+	password string	
+}
+
+func NewDatabase() struct{
+	
+}
---
+++ develop/dev11/internal/server/handler.go
@@ -0,0 +1,7 @@
+package server
+
+
+type Server struct{
+	host string
+	port string
+}
