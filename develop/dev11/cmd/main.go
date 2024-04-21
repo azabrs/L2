1ac289f38657b7b27e3f1df6971821b50dbc083d
azabrs <Apukha@mail.ru>
2024-04-21 16:16:18 +0300

    add all methods and start test

--- develop/dev11/cmd/main.go
+++ develop/dev11/cmd/main.go
@@ -1,8 +1,29 @@
 package main
 
-import ()
+import (
+	"L2/develop/dev11/internal/config"
+	"L2/develop/dev11/internal/database"
+	"L2/develop/dev11/internal/server"
+	"L2/develop/dev11/internal/services"
+	"log"
+)
 
 
 
 func main(){
+	v, err := config.LoadConfig()
+	if err != nil{
+		log.Fatal(err)
+	}
+	conf, err := config.ParseConfig(v)
+	if err != nil{
+		log.Fatal(err)
+	}
+	db, err := database.NewDatabase(conf.Postgres)
+	if err != nil{
+		log.Fatal(err)
+	}
+	service := services.NewService(db)
+	server := server.NewServer(conf.Server, service)
+	server.StartServer()
 }
