87605b31f69265cfd8d4b17381d4a940cc6ef0d7
azabrs <Apukha@mail.ru>
2024-04-25 20:34:59 +0300

    add 7 8 patterns

---
+++ pattern/07_strategy.go
@@ -0,0 +1,84 @@
+// Package strategy is an example of the Strategy Pattern.
+package main
+
+import "fmt"
+
+// StrategySort provides an interface for sort algorithms.
+type StrategySort interface {
+	Sort([]int)
+}
+
+// BubbleSort implements bubble sort algorithm.
+type BubbleSort struct {
+}
+
+// Sort sorts data.
+func (s *BubbleSort) Sort(a []int) {
+	size := len(a)
+	if size < 2 {
+		return
+	}
+	for i := 0; i < size; i++ {
+		for j := size - 1; j >= i+1; j-- {
+			if a[j] < a[j-1] {
+				a[j], a[j-1] = a[j-1], a[j]
+			}
+		}
+	}
+}
+
+// InsertionSort implements insertion sort algorithm.
+type InsertionSort struct {
+}
+
+// Sort sorts data.
+func (s *InsertionSort) Sort(a []int) {
+	size := len(a)
+	if size < 2 {
+		return
+	}
+	for i := 1; i < size; i++ {
+		var j int
+		var buff = a[i]
+		for j = i - 1; j >= 0; j-- {
+			if a[j] < buff {
+				break
+			}
+			a[j+1] = a[j]
+		}
+		a[j+1] = buff
+	}
+}
+
+// Context provides a context for execution of a strategy.
+type Context struct {
+	strategy StrategySort
+}
+
+// Algorithm replaces strategies.
+func (c *Context) Algorithm(a StrategySort) {
+	c.strategy = a
+}
+
+// Sort sorts data according to the chosen strategy.
+func (c *Context) Sort(s []int) {
+	c.strategy.Sort(s)
+}
+
+func main(){
+	data1 := []int{8, 2, 6, 7, 1, 3, 9, 5, 4}
+	data2 := []int{8, 2, 6, 7, 1, 3, 9, 5, 4}
+
+	ctx := new(Context)
+
+	ctx.Algorithm(&BubbleSort{})
+
+	ctx.Sort(data1)
+
+	ctx.Algorithm(&InsertionSort{})
+
+	ctx.Sort(data2)
+
+	fmt.Println(data1)
+	fmt.Println(data2)
+}
